52bf1dfd6ac594a0f4d50bd27dcb4461555b1335
lreaderl <lllreaderlll@gmail.com>
2019-10-29 18:39:23 -0400

    create card

--- Card/src/test/java/edu/norwich/cs509/beta/LambdaFunctionHandlerTest.java
+++ Card/src/test/java/edu/norwich/cs509/beta/LambdaFunctionHandlerTest.java
@@ -20,6 +20,8 @@ import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
 
+import edu.norwich.cs509.card.LambdaFunctionHandler;
+
 /**
  * A simple test harness for locally invoking your Lambda function handler.
  */
